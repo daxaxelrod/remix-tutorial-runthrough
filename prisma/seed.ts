e10d1e2f99ddd22a4ba37e129fa2128345df4aa9
David Axelrod <daxaxelrod@gmail.com>
2022-12-22 18:52:06 -0500

    user auth progress and more readme notes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,9 +2,19 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function seed() {
+  const karl = await db.user.create({
+    data: {
+      username: "karl",
+      // this is a hashed version of "twixrox"
+      passwordHash:
+        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+    },
+  });
+
   await Promise.all(
     getJokes().map((joke) => {
-      return db.joke.create({ data: joke });
+      const data = { jokesterId: karl.id, ...joke };
+      return db.joke.create({ data });
     })
   );
 }
